f26515a089e2833bf51db1b0660093e7e9d6050e
Hector Zamudio <zamudio1243@gmail.com>
2019-11-11 14:45:14 -0600

    Reportes

--- src/avidos/Caballo.java
+++ src/avidos/Caballo.java
@@ -1,4 +1,4 @@
-package greedy;
+package avidos;
 /*
 Hector octubre 2019
 Heuristica: Mover el caballo a la posicion con menor numero de posibles movimientos
--- src/avidos/Coordenada.java
+++ src/avidos/Coordenada.java
@@ -1,4 +1,4 @@
-package greedy;
+package avidos;
 /*
 Hector octubre 2019 
 */
